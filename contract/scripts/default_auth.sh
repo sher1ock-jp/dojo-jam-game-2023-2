9661357847f611b7856bbaabdcddd8a83befc0f3
Takayuki Ono <takaaaaadayo@gmail.com>
2023-10-22 16:05:12 +0530

    add sozo auth

--- contract/scripts/default_auth.sh
+++ contract/scripts/default_auth.sh
@@ -2,17 +2,27 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export WORLD_ADDRESS="0x7d1f066a910bd86f532fa9ca66766722c20d47462fb99fb2fb0e1030262f9c5";
+# export RPC_URL="http://localhost:5050";
+export WORLD_ADDRESS="0x7bfc4dc34e5e8a3c7c29604134dbf6e0e9f133aa6431d1aa69d5f08164e67b6";
+# export PLAYER_ADDRESS="0x051F533167D366AeE5AEbF95B85d8cd5a5A4Ef4A";
+# export ADDRESS="0x051F533167D366AeE5AEbF95B85d8cd5a5A4Ef4A";
 
-# enable system -> component authorizations
-COMPONENTS=("Position" "Moves" )
+# Read the System to Components JSON file
+models_json=$(cat ./scripts/system_models.json)
+contract_address_json=$(cat ./scripts/contract_address.json)
 
-for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component spawn --world $WORLD_ADDRESS
+models_json=$(cat ./scripts/system_models.json)
+contract_address_json=$(cat ./scripts/contract_address.json)
+
+# Loop through each system
+for system in $(echo $models_json | jq -r 'keys[]'); do
+    # Loop through each component that the system writes to
+    for model in $(echo $models_json | jq -r ".$system[]"); do
+        contract_address=$(echo "$contract_address_json" | jq -r ".$system[0]")
+        # make the system a writer of the component
+        commands+=("sozo auth writer --world "$WORLD_ADDRESS" $model $contract_address")
+    done
 done
 
-for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component move --world $WORLD_ADDRESS
-done
 
 echo "Default authorizations have been successfully set."
